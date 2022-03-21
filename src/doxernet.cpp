663c457c51cb5aeace1690d1648f67c62e8a4e9e
ElandaOfficial <elendaofficial@gmail.com>
2022-03-21 01:41:56 +0100

    Initial setup

---
+++ src/doxernet.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+#include <juce_core/juce_core.h>
+
+class DoxerNet
+{
+public:
+private:
+};
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
